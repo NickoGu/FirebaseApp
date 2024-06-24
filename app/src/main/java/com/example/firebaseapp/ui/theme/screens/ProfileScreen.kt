7cc6f464d4a8dbcc0c841705e2089588cb25ba47
NickoGu <92960428+NickoGu@users.noreply.github.com>
2024-06-24 17:45:21 -0300

    🎨 Change UI profileScreen

--- app/src/main/java/com/example/firebaseapp/ui/theme/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/firebaseapp/ui/theme/screens/ProfileScreen.kt
@@ -15,11 +15,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -30,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.core.net.toUri
@@ -56,7 +59,7 @@ fun ProfileScreen(
             selectedImageUri = uri
         }
     Scaffold(topBar = {
-        TopAppBar(title = { Text("Editar Perfil") }, actions = {
+        TopAppBar(title = { Text("Editar Perfíl") }, actions = {
             Icon(
                 Icons.Rounded.Refresh,
                 contentDescription = "",
@@ -73,7 +76,7 @@ fun ProfileScreen(
                     .fillMaxWidth()
                     .fillMaxHeight(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.SpaceEvenly,
         ) {
             AsyncImage(
                 model =
@@ -92,7 +95,7 @@ fun ProfileScreen(
                         .clip(shape = RoundedCornerShape(100)),
                 contentScale = ContentScale.Crop,
             )
-            TextField(
+            OutlinedTextField(
                 value =
                     if (userName == "") {
                         userName = viewModel.userInfo.userName
@@ -103,29 +106,54 @@ fun ProfileScreen(
                 onValueChange = { newName ->
                     userName = newName
                 },
-                placeholder = { Text(text = viewModel.userInfo.userName) },
-                modifier = Modifier.padding(top = 16.dp),
+                shape = RoundedCornerShape(64.dp),
+                placeholder = { Text(text = "Nombre de usuario") },
+                modifier = Modifier.padding(vertical = 16.dp),
+                colors =
+                    OutlinedTextFieldDefaults.colors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        cursorColor = Color.White,
+                        focusedLabelColor = Color.White,
+                        focusedContainerColor = Color(0xFF3d4354),
+                        unfocusedContainerColor = Color(0xFF3d4354),
+                    ),
             )
-            Button(
-                onClick = {
-                    launcher.launch("image/*")
-                },
-                modifier = Modifier.padding(top = 16.dp),
-            ) {
-                Text("Seleccionar Imagen")
-            }
+            Column {
+                Button(
+                    onClick = {
+                        launcher.launch("image/*")
+                    },
+                    modifier = Modifier.padding(horizontal = 12.dp).fillMaxWidth(),
+                    colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF0232c39),
+                            contentColor = Color.White,
+                        ),
+                ) {
+                    Text("Seleccionar imágen")
+                }
 
-            Button(
-                onClick = {
-                    selectedImageUri?.let { uri ->
-                        viewModel.uploadImage(uri)
-                        viewModel.updateName(userName)
-                        navController.navigate("fireStore")
-                    }
-                },
-                modifier = Modifier.padding(top = 16.dp),
-            ) {
-                Text("Guardar Usuario")
+                Button(
+                    onClick = {
+                        selectedImageUri?.let { uri ->
+                            viewModel.uploadImage(uri)
+                            viewModel.updateName(userName)
+                            navController.navigate("fireStore")
+                        }
+                    },
+                    colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor =
+                                Color(
+                                    0xFF232c39,
+                                ),
+                            contentColor = Color.White,
+                        ),
+                    modifier = Modifier.padding(horizontal = 12.dp).fillMaxWidth(),
+                ) {
+                    Text("Guardar usuario")
+                }
             }
         }
     }
